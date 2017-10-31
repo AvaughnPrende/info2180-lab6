4c2febb206c2cf046395fb280d48d5fa10f758bb
Avaughn Prendergast <avaughn.prendergast@mymona.uwi.edu>
2017-10-31 08:48:28 +0000

    Partially working

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-$(document).ready(function(){
-    let searchButton = $("#search-button");
-    let url = "request.php";
+window.onload = function(){
+    let httpRequest;
+    let searchButton;
+    let url;
     
-    searchButton.on("click",function(){
-        $.ajax(url,
-        {
-            success: function(){
-                alert("SUCCESS");
-            },
-            error: function(){
-                alert("FAILURE");
-            }
-        })
+    searchButton = $("#search-button");
+   
+    
+    searchButton.click(function(element){
+        element.preventDefault();
+        url          = "request.php";
+        httpRequest  = new XMLHttpRequest();
+        httpRequest.onreadystatechange = alertDef;
+        httpRequest.open('GET',url);
+        httpRequest.send();
+        
     })
-})
-       
+    
+    function alertDef(){
+        if(httpRequest.readyState === XMLHttpRequest.DONE){
+            if(httpRequest.status === 200){
+                alert(response);
+            }
+        }
+        else{
+            alert("READY STATE: " + httpRequest.readyState + "   --- STATUS: " + httpRequest.status);
+        }
+    }
+}   
