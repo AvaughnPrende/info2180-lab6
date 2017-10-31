2cdb8c3eee4b86a7022312b0c8020c214d6351fb
Avaughn Prendergast <avaughn.prendergast@mymona.uwi.edu>
2017-10-31 11:09:23 +0000

    Partially complete ex 2

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
+let httpRequest;
+let searchButton;
+let url;
+let response;
+
 window.onload = function(){
-    let httpRequest;
-    let searchButton;
-    let url;
-    let response;
-    
     searchButton = $("#search-button");
    
     
     searchButton.on("click",function(element){
         element.preventDefault();
-        url          = "https://info2180-lab6-avaughnprende.c9users.io/request.php?q=definition";
+        //url          = "https://info2180-lab6-avaughnprende.c9users.io/request.php?q=definition";
+        url            = "request.php";
         httpRequest  = new XMLHttpRequest();
         httpRequest.onreadystatechange = alertDef;
-        httpRequest.open('GET',url);
+        httpRequest.open('GET',url,true);
         httpRequest.send();
+        response = httpRequest.responseText;
         
     })
     
     function alertDef(){
         if(httpRequest.readyState === XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
-                let x = httpRequest.repsonseText;
-                alert(x);
+                callback(httpRequest.responseText);
             }
         }
         else{
             //alert("READY STATE: " + httpRequest.readyState + "   --- STATUS: " + httpRequest.status);
         }
+    }
+    function callback(x){
+        alert(x);
     }
 }   
