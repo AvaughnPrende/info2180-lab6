5492e64334e937bc395a79eed331827311cf1d7e
Avaughn Prendergast <avaughn.prendergast@mymona.uwi.edu>
2017-11-01 20:57:51 +0000

    EDIT- No request sent when search bar is blank

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-let httpRequest;
-let xmlRequest;
-let searchButton;
-let url;
-let response;
-let searchTerm
-let fetchSearchTerm
-let a;
-
 window.onload = function(){
    let searchButton = $("#search-button");
    let searchBar    = $("#search-bar");
    let result       = $("#result");
+   let getAllButton = $("#get-all");
+   
+   let httpRequest;
+   let xmlRequest;
+   let url;
    let searchTerm;
     
     searchButton.on("click",function(element){
@@ -25,42 +21,21 @@ window.onload = function(){
     });
     
     function alertDef(){
-        if(httpRequest.readyState === XMLHttpRequest.DONE){
-            if(httpRequest.status === 200){
-                if(searchResultFound(httpRequest.responseText)){
-                    callback(httpRequest.responseText);
-                }
-                else{
-                    callback("'" + searchTerm +  "'" + " NOT FOUND");
+        if (!(fetchSearchTerm()==='')){//Don't send AJAX request if the search bar is empty
+            if(httpRequest.readyState === XMLHttpRequest.DONE){
+                if(httpRequest.status === 200){
+                    if(searchResultFound(httpRequest.responseText)){
+                        callback(httpRequest.responseText);
+                    }
+                    else{
+                        callback("'" + searchTerm +  "'" + " NOT FOUND");
+                    }
                 }
             }
         }
     }
-  
-    function callback(string){
-        result.html(string);
-    }
-    
-    /*
-    function stripTags(string){
-        string = string.replace("<p>","");
-        string = string.replace("</p>","\n");
-        string = string.replace("<h3>","");
-        string = string.replace("</h3>","\n");
-        return string;
-    }
-    */
-    
-     fetchSearchTerm = function(){
-        return document.getElementById("search-bar")[0].value;
-    }
-    
-    function searchResultFound(string){
-        return !string.endsWith("<p></p>");
-    }
-    
-    
-    $("#get-all").on("click", function(element){
+ 
+    getAllButton.on("click", function(element){
         element.preventDefault();
         url                            = "https://info2180-lab6-avaughnprende.c9users.io/request.php?q=&all=true";
         xmlRequest                     = new XMLHttpRequest();
@@ -73,10 +48,8 @@ window.onload = function(){
     function displayAllDefinitions(){
         if(xmlRequest.readyState === XMLHttpRequest.DONE){
             if(xmlRequest.status === 200){
-                result.empty();
-               createList();
-            }
-            else{
+                result.empty();//Claer the div before entering new data
+                createList();
             }
         }
     }
@@ -90,5 +63,17 @@ window.onload = function(){
             let content  = $(`<h3>${i+1}. ${defTerm}</h3><p>${defData}<br>-- ${author}</p>`);
             result.append(content);
         };
+    }
+    
+    function callback(string){
+        result.html(string);
+    }
+    
+     function fetchSearchTerm(){
+        return document.getElementById("search-bar")[0].value;
+    }
+    
+    function searchResultFound(string){
+        return !string.endsWith("<p></p>");
     }
 };
